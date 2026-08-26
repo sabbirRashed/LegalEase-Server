800ed5b9fdfde8d270e35e3645cea8844e2e9635
sabbirRashed <srsabbirrahman12@gmail.com>
2026-08-26 22:51:40 +0600

    feat: initialize express server and connect mongodb

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('LegalEase');
+        const userCollection = db.collection('user')
+        const commentsCollection = db.collection('comments')
+
+        app.get('/api/users', async(req, res,)=>{
+
+        })
+
+        app.post('/api/comments', async(req, res)=>{
+            
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`LegalEase is listening on port ${port}`)
+})
